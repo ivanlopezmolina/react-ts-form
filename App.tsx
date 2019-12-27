10dbeb80d422b83e0e03c6eb6c599d970c840a86
ivanlopezmolina <ivanlopezmolina@gmail.com>
2019-12-26 23:08:19 -0500

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import User from './pages/Users';
+// import 'App.css';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <User />
+    </div>
+  );
+}
+
+export default App;
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-mprdwf
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-mprdwf)
